67eb1da15b83a3116584b7f37493df2e9bb63021
imNima10 <x1nima1x@gmail.com>
2025-10-29 16:14:52 +0330

    Add handler for 'home' callback and show translation service keyboard

--- actions/index.js
+++ actions/index.js
@@ -11,3 +11,23 @@ exports.startMenu = (bot, chatId) => {
 
     bot.sendMessage(chatId, "به ربات ترجمه خوش اومدی!", inlineKeyboard)
 }
+
+exports.homeMenu = (bot, chatId,messageId) => {
+    let inlineKeyboard = {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    { text: 'Google 🇺🇸', callback_data: 'Google' },
+                    { text: 'Microsoft 🇺🇸', callback_data: 'microsoft' },
+                    { text: 'Yandex 🇷🇺', callback_data: 'yandex' }
+                ]
+            ]
+        }
+    };
+
+    bot.editMessageText("لطفا سرویس خود را انتخاب کنید!", {
+        chat_id: chatId,
+        message_id: messageId,
+        reply_markup: inlineKeyboard.reply_markup
+    })
+}
--- index.js
+++ index.js
@@ -2,3 +2,10 @@ let bot = require("./bot")
 let actions = require("./actions")
 
 bot.onText(/\/start/, (msg) => actions.startMenu(bot, msg.chat.id))
+
+bot.on('callback_query', (query) => {
+    let command = query.data;
+    let chatId = query.message.chat.id;
+    let messageId = query.message.message_id;
+    if (command === 'home') actions.homeMenu(bot, chatId, messageId)
+})
